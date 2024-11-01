a05602824d000ffac217e39f45cc185696e3b916
Caio Cesar Madeira Mattoso De Souza <24280006@facin.portoalegre.pucrsnet.br>
2024-11-01 08:30:24 -0300

    remove logic/uml concepts

--- aula241024~3.sql
+++
@@ -1,7 +0,0 @@
-select matricula from alunos where matricula > 21103839 order by matricula;
-select sysdate from dual;
-select matricula, idade from alunos order by idade;
-select nome, sexo from alunos where curso = '98AJ ' and idade + 1 = 24;
-select avg(idade) from alunos;
-select count(*), avg(nota) from historico where situacao != 'Ativo';
-select nome from alunos order by nome DESC;
